81c7f529aa078b850c85cc9cacf818eb455d6e81
Hao Jie <aspiringcatlover@gmail.com>
2021-04-17 17:06:10 +0800

    Added express and nodemon

--- app/index.js
+++ app/index.js
@@ -1,14 +1,10 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const server = app.listen(7000, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
